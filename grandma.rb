e8cf1d609025f1055b6871ed8e5313e09cdc8205
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-19 21:25:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++
@@ -1,8 +0,0 @@
-def speak_to_grandma(phrase = "")
-   if phrase == "I LOVE YOU GRANDMA!"
-     puts "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-end
